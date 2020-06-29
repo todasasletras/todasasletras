3b5d199fb8dbfa3f26077b6eb277cc96a82373c6
lucasjs <lucas.tcjb@gmail.com>
2020-06-29 01:04:18 -0300

    events: refactor june events

--- src/data/events.js
+++ src/data/events.js
@@ -1,19 +1,19 @@
-import liveJun from 'assets/cover/live-jun.png';
 import eventJune from 'assets/cover/event-jun.png';
+import eventJuly from 'assets/cover/event-jul.png';
 
 const events = [
   {
-    img: liveJun,
-    name: 'Todas as Lives #2',
-    description: 'Linguagem neutra',
-    url: 'https://www.meetup.com/pt-BR/LGBTIinTech-SP/events/271267181',
-    date: '18 de junho',
+    img: eventJune,
+    name: 'Meetup #15',
+    description: 'Do Village ao Arouche: o movimento LGBTQIA+ nos EUA e Brasil',
+    url: 'https://www.meetup.com/pt-BR/todasasletras/events/271586322',
+    date: '30 de junho',
     location: 'Online',
     active: true,
   },
   {
-    img: eventJune,
-    name: 'Meetup #15',
+    img: eventJuly,
+    name: 'Meetup #16',
     description: 'A definir',
     url: '#',
     date: 'A definir',
