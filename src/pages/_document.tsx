0d625ac51186a1135f2aef6adb0972b07fdd174e
Lucas Silva <lucas.tcjb@gmail.com>
2022-03-24 00:05:03 -0300

    immersion: add project landing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,7 +30,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
